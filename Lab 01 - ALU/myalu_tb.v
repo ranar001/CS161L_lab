5facc21a133a196b6dc46f22d99f6bd14f938cf3
Ronny Anariva <49249431+ranar001@users.noreply.github.com>
2021-04-17 14:08:42 -0700

    editing the testbench file

--- Lab 01 - ALU/myalu_tb.v
+++ Lab 01 - ALU/myalu_tb.v
@@ -1,11 +1,11 @@
 //=========================================================================
 // Name & Email must be EXACTLY as in Gradescope roster!
-// Name: 
-// Email: 
+// Name:Ronny Anariva
+// Email: ranar001@ucr.edu
 // 
-// Assignment name: 
-// Lab section: 
-// TA: 
+// Assignment name: Prelab 01
+// Lab section: 021
+// TA: Yujia Zhai 
 // 
 // I hereby certify that I have not received assistance on this assignment,
 // or used code, from ANY outside source other than the instruction team
@@ -76,7 +76,7 @@ module myalu_tb;
         opcode = 3'b000; 
         totalTests = totalTests + 1;
         $write("\tTest Case 1.1: Unsigned Add ... ");
-        A = 8'hFF;
+        A = 8'hFF; 
            B = 8'h01;
         R = 8'h00; 
         #100; // Wait 
@@ -89,63 +89,136 @@ module myalu_tb;
         #10; // Wait 
         
 		// Add more tests here
-
+			$write("Test Case: 1.2: Unsigned Add \n");
+			A= 8'h0A; //A= 0000 1010 
+			B= 8'h11; //B= 0001 0001
+			R= 8'h1B; //R= 0001 1011 
+            //10+17=27
+			if (R != result || zero != 1'b1 || carryout != 1'b1) begin
+            $write("failed\n");
+            failedTests = failedTests + 1;
+        end else begin
+            $write("passed\n");
+        end
+        #10; // Wait 
         // ---------------------------------------------
         // Testing unsigned subs 
         // --------------------------------------------- 
         $write("Test Group 2: Testing unsigned subs ...\n");
         opcode = 3'b010; 
-        
 		// Add more tests here
-
+			A= 8'hB1; //A= 1011 0001 
+			B= 8'h20; //B= 0010 0000
+			R= 8'h91; //R= 1001 0001
+            //177-32=145 
+			if (R != result || zero != 1'b1 || carryout != 1'b1) begin
+            $write("failed\n");
+            failedTests = failedTests + 1;
+        end else begin
+            $write("passed\n");
+        end
+        #10; // Wait  
         // ---------------------------------------------
         // Testing signed adds 
         // --------------------------------------------- 
         $write("Test Group 3: Testing signed adds ...\n");
         opcode = 3'b001; 
-
 		// Add more tests here
-
+            //85+(-9)=76
+            A=8'h55;//A=0101 0101
+            B=8'hF6;//B=1111 0110  -->after using 2s complement 
+            R=8'h4C;//R=0100 1100
+			if (R != result || zero != 1'b1 || carryout != 1'b1) begin
+            $write("failed\n");
+            failedTests = failedTests + 1;
+        end else begin
+            $write("passed\n");
+        end
+        #10; // Wait 
+        
         // ---------------------------------------------
         // Testing signed subs 
         // --------------------------------------------- 
         $write("Test Group 4: Testing signed subs ...\n");
         opcode = 3'b011; 
-                
 		// Add more tests here
-
+        // -10+25=15
+			A= 8'hF5; //A= 1111 0101 --> 2s complement 
+			B= 8'h19; //B= 0001 1001
+			R= 8'h0F; //R= 0000 1111 
+			if (R != result || zero != 1'b1 || carryout != 1'b1) begin
+            $write("failed\n");
+            failedTests = failedTests + 1;
+        end else begin
+            $write("passed\n");
+        end
+        #10; // Wait  
         // ---------------------------------------------
         // Testing ANDS 
         // --------------------------------------------- 
         $write("Test Group 5: Testing ANDs ...\n");
         opcode = 3'b100; 
-                
 		// Add more tests here
-
+			A= 8'hFF; //A= 1111 1111 
+			B= 8'h00; //B= 0000 0000
+			R= 8'h00; //R= 0000 0000 
+			if (R != result || zero != 1'b1 || carryout != 1'b1) begin
+            $write("failed\n");
+            failedTests = failedTests + 1;
+        end else begin
+            $write("passed\n");
+        end
+        #10; // Wait 
+        
         // ----------------------------------------
         // ORs 
         // ---------------------------------------- 
         $write("Test Group 6: Testing ORs ...\n");
         opcode = 3'b101; 
-        
 		// Add more tests here
-        
+        A= 8'hAA; //1010 1010
+        B= 8'hD7; //1101 0111
+        R= 8'hFF; //1111 1111
+        if (R != result || zero != 1'b1 || carryout != 1'b1) begin
+            $write("failed\n");
+            failedTests = failedTests + 1;
+        end else begin
+            $write("passed\n");
+        end
+        #10 //wait
         // ----------------------------------------
         // XORs 
         // ---------------------------------------- 
         $write("Test Group 7: Testing XORs ...\n");
         opcode = 3'b110; 
-        
 		// Add more tests here
-        
+        A= 8'h0F; //A=0000 1111
+        B= 8'hF0; //B=1111 0000
+        R= 8'hFF; //R=1111 11111
+        if (R != result || zero != 1'b1 || carryout != 1'b1) begin
+            $write("failed\n");
+            failedTests = failedTests + 1;
+        end else begin
+            $write("passed\n");
+        end
+        #10 //wait
+
         // ----------------------------------------
         // Div 2 
         // ----------------------------------------
         $write("Test Group 8: Testing DIV 2 ...\n");
         opcode = 3'b111; 
-        
 		// Add more tests here
-
+        //26/2=13
+        A= 8'h1A; //A=0001 1010
+        R= 8'h0C; //R=0000 1101 --> shift right by 1 bit
+        if (R != result || zero != 1'b1 || carryout != 1'b1) begin
+            $write("failed\n");
+            failedTests = failedTests + 1;
+        end else begin
+            $write("passed\n");
+        end
+        #10 //wait
         // -------------------------------------------------------
         // End testing
         // -------------------------------------------------------
